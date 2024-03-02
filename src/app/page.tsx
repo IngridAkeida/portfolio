37809207766b6080ee0bbcb899d4f8b1b577b590
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-02 23:22:32 +0100

    update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,62 @@
 "use client"
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import TechnologiesList from '@/components/TechnologiesList';
 import Image from 'next/image';
 
 import { CiDesktopMouse2 } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
 
 
-const App: React.FC = (darkMode) => {
-  
-
+const App: React.FC = ({darkMode, technologies}) => {
   return (      
-    <div className={`bg-white dark:bg-gray-800 transition-colors duration-500 ${darkMode && 'dark'} p-6 md:p-0 flex flex-col mx-auto lg:max-w-5xl`}>
+    <div className={`bg-gray-100 text-cyan-900 dark:bg-cyan-900 dark:text-gray-100 transition-colors duration-500 ${darkMode && 'dark'} p-6 md:p-0 flex flex-col mx-auto lg:max-w-5xl`}>
       <header>
         <Navbar /> 
         <Sidebar />
       </header>
-      <main className="container mx-auto py-8 px-4 min-h-screen p-24 grid grid-cols-4 gap-4">  
+      <main className="container mx-auto py-8 px-4 min-h-screen p-24 grid grid-cols-6 gap-4">  
       <div className='p-4'></div>
-      <div className='p-4 col-span-3'>
+      <div className='p-4 col-span-5'>
         <div className=' h-screen flex flex-col gap-4 justify-center items-center'>
-            <div className='flex items-center gap-4'>
-              <div>
+            <div className='grid grid-cols-5 gap-4 items-center'>
+              <div className='col-span-3'>
                 <h1 className="text-2xl font-bold">Hi, {`I'm`} Ingrid Sanches</h1>
                 <h2 className="text-md font-bold">Front End Developer</h2>
-                <p>{`I'm`} a business grad {`who's`} taken a detour into the dazzling world of Tech ⭐️!</p>
-                <p>Yes, from processing, performance, usability, accessibility and line coding.</p>
+                <p className='mt-8'>{`I'm`} a business grad {`who's`} taken a detour into the dazzling world of Tech ⭐️!</p>
+                <p className='mt-4'>Yes, from processing, performance, usability, accessibility and line coding.</p>
+                <button className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-8">
+                Contact me
+                </button>
               </div>
-              <div className=''>
+              <div className='col-span-2'>
                 <Image src="/assets/bio-image.png" alt="Ingrid Sanches Perfil image"
                 className='rounded-full' 
-                width={200} height={200}/>
+                width={400} height={400}/>
               </div>
             </div>
-            <div className='flex items-center gap-2 mt-28'> <CiDesktopMouse2 /> Scroll down <IoIosArrowDown /></div>
+            <div className='flex items-center gap-2 mt-28'>   
+              <CiDesktopMouse2 size={30}/> 
+              Scroll down 
+              <IoIosArrowDown size={30}/>
+            </div>
             
             
         </div>
         <div className='h-screen flex flex-col gap-4 justify-center items-center'>
           <div>
-            <h3>about me:</h3>
+            <h3 className="text-2xl font-bold mb-2">About me:</h3>
             <article className="text-md">
-            
-            Yes, from processing, performance, usability, accessibility and line coding, I am now a student at Hyper Island, working as a freelancer in the digital universe. {`I'm`} currently diving head first into React and TypeScript 🌱, but hey, {`I'm`} ready for any project! After all, learning is my game 💓. Oh, and did I mention? I love cats and horror movies 🐱👻. So expect some spooky surprises in my projects! 😉
+              Yes, from processing, performance, usability, accessibility and line coding, I am now a student at Hyper Island, working as a freelancer in the digital universe. {`I'm`} currently diving head first into React and TypeScript 🌱, but hey, {`I'm`} ready for any project! After all, learning is my game 💓. Oh, and did I mention? I love cats and horror movies 🐱👻. So expect some spooky surprises in my projects! 😉
             </article>
           </div>
           <div>
-            <h3>Skills:</h3>
-            <p className="text-lg mb-8">
-            </p>
+          <h3 className="text-2xl font-bold mb-2">Skills:</h3>
+          <article className="text-md">
+          
+          </article>
+            <TechnologiesList technologies={technologies}/>
+            
           </div>
         </div>
         
--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -33,7 +33,7 @@ const Menus: React.FC = () => {
   ]
 
   return(
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
         {menus.map((menu, index)=>(
           <li key={index}>
             <a href={menu.menuHref} className={menu.menuStyle}>{menu.menuName}</a>
