ad84faefc116513deac14ee6287756761ba99726
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-02 17:01:10 +0100

    create - logo file

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+const Logo: React.FC = () => {
+
+  return(
+    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <span className=" text-md font-semibold whitespace-nowrap dark:text-white px-2 rounded-md border">{`<IS/>`}</span>
+    </a>
+  )
+}
+
+export default Logo; 
