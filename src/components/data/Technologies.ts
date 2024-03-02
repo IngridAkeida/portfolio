74c5c4506de91273a124ecf756ce18587dc72a0e
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-02 23:21:34 +0100

    create - technologies data component

---
+++ src/components/data/Technologies.ts
@@ -0,0 +1,71 @@
+
+const Technologies = [
+    {
+        name: 'JavaScript',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+      },
+      {
+        name: 'CSS3',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
+        link: 'https://www.w3schools.com/css/'
+      },
+      {
+        name: 'HTML5',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+        link: 'https://www.w3.org/html/'
+      },
+      {
+        name: 'Bash',
+        image: 'https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg',
+        link: 'https://www.gnu.org/software/bash/'
+      },
+      {
+        name: 'Node.js',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
+        link: 'https://nodejs.org'
+      },
+      {
+        name: 'React',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+        link: 'https://reactjs.org/'
+      },
+      {
+        name: 'Bootstrap',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg',
+        link: 'https://getbootstrap.com'
+      },
+      {
+        name: 'GraphQL',
+        image: 'https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg',
+        link: 'https://graphql.org'
+      },
+      {
+        name: 'TypeScript',
+        image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg',
+        link: 'https://www.typescriptlang.org/'
+      },
+      {
+        name: 'Next.js',
+        image: 'https://cdn.worldvectorlogo.com/logos/nextjs-2.svg',
+        link: 'https://nextjs.org/'
+      },
+      {
+        name: 'Supabase',
+        image: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/299/square_480/supabase-logo-icon_1.png',
+        link: 'https://supabase.com/'
+      },
+      {
+        name: 'Styled Components',
+        image: 'https://raw.githubusercontent.com/styled-components/brand/master/styled-components.png',
+        link: 'https://styled-components.com/'
+      },
+      {
+        name: 'Figma',
+        image: 'https://www.vectorlogo.zone/logos/figma/figma-icon.svg',
+        link: 'https://www.figma.com/'
+      }
+  
+];
+
+export default Technologies;
