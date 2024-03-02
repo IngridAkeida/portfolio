f441f6f88bb8e413710983710bed01dba3cb4a5f
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-03 00:07:14 +0100

    fix - dark mode

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo: React.FC = () => {
 
   return(
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <span className=" text-black text-md font-semibold whitespace-nowrap dark:text-white px-2 rounded-md border">{`<IS/>`}</span>
+        <span className=" text-cyan-900 dark:text-gray-100 bg-gray-100 dark:bg-cyan-900 text-md font-semibold whitespace-nowrap  px-2 rounded-md text-xl">{`<IS/>`}</span>
     </a>
   )
 }
