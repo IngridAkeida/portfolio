f505f5eede10a4ca2fded055c727e9a70fc92d3a
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-02 23:54:59 +0100

    add - protocol for remote partterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd2eip9sf3oo6c2.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      }
+    ],   
+  },
+};
 
 export default nextConfig;
