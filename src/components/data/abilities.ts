0abeca7eb52d0b87b524df9efaf6781415f66567
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-06 10:19:32 +0100

    create - abilities object

---
+++ src/components/data/abilities.ts
@@ -0,0 +1,17 @@
+import { FaComments, FaTools, FaRegListAlt, FaUserCog, FaClock, FaUserFriends, FaLightbulb, FaBrain, FaUserCheck, FaBook } from 'react-icons/fa';
+
+
+const abilities = [
+  { icon: FaComments, skill: "Communication" },
+  { icon: FaTools, skill: "Problem-solving" },
+  { icon: FaRegListAlt, skill: "Attention to detail" },
+  { icon: FaUserCog, skill: "Adaptability" },
+  { icon: FaClock, skill: "Time management" },
+  { icon: FaUserFriends, skill: "Teamwork" },
+  { icon: FaLightbulb, skill: "Creativity" },
+  { icon: FaBrain, skill: "Critical thinking" },
+  { icon: FaUserCheck, skill: "Attention to user experience" },
+  { icon: FaBook, skill: "Continuous learning" }
+];
+
+export default abilities;
