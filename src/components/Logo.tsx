7c65686489e6c3e5e082d5f62a452de88c89e4b0
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 09:03:12 +0100

    delete - elements inside Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,8 @@
-import { IoLogoOctocat } from "react-icons/io";
-
 const Logo: React.FC = () => {
   return (
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
       <span className="flex text-cyan-900 dark:text-gray-100 bg-gray-100 dark:bg-cyan-900 text-md font-semibold whitespace-nowrap p-2 rounded-md text-xl">
-        iS <IoLogoOctocat/>
+        {`Ingrid's Protfolio`}
       </span>
     </a>
   );
