62b49e222dd5ce99d368fa756ea5ce6bff368be5
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 09:04:01 +0100

    add - footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer: React.FC = () => {  
+  return(
+    <footer className="py-4 px-8 text-center text-gray-600 dark:text-gray-400">
+        <div>
+          <h3>Contact me:</h3>
+          <p className="text-lg mb-8">
+          </p>
+        </div>
+        <div>
+          © {new Date().getFullYear()} Ingrid Sanches. All rights reserved.
+        </div>
+      </footer>
+  )
+}
+
+export default Footer;
