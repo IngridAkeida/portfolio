4ee679f9f4dd6d77b33d637c78a77ad11e1b3b1d
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 08:58:50 +0100

    change - some styles and elements

--- src/components/AbilitiesList.tsx
+++ src/components/AbilitiesList.tsx
@@ -10,15 +10,17 @@ interface AbilitiesProps {
 const AbilitiesList : React.FC<AbilitiesProps> = ({ abilities }) => {
   
   return (
-    <ul className="flex justify-left">
-      {abilities.map((ability, index) => (
-        <li key={index}>
-            <ability.icon size={60}/>
-            <p>{ability.skill}</p>
-         
-        </li>
-      ))}     
-    </ul>
+    <div>
+      <h4 className="text-xl font-bold mt-2 mb-1">Soft Skills</h4>
+      <ul className="flex flex-wrap justify-center md:justify-start">
+        {abilities.map((ability, index) => (
+          <li key={index} className="flex flex-col items-center justify-center md:justify-start mx-2 my-2">
+              <ability.icon size={30}/>
+              <p className="mt-2">{ability.skill}</p>
+          </li>
+        ))}     
+      </ul>
+    </div>
   );
 }
 
