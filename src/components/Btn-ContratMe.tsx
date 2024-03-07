34a848eee5325c974ce7b49ca15f2a42743a7934
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 08:59:12 +0100

    add - btn contratme

---
+++ src/components/Btn-ContratMe.tsx
@@ -0,0 +1,9 @@
+const BtnContratMe: React.FC = () => {
+  return (
+    <button className="animate-pulse bg-cyan-300 hover:bg-blue-700  py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      Cont<span className="rover:hidden rover:font-bold rover:text-green-100">R</span>act me!
+    </button>
+  );
+};
+
+export default BtnContratMe;
