2b6c07b98a3e4e477b8ae3e61b1b9f6b71fb2655
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 09:02:39 +0100

    delete - unuse elements on Menus

--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -1,12 +1,11 @@
 const Menus: React.FC = () => {
 
-  const munuTailwindHome = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-gray-100 md:dark:text-blue-500";
-  const munuTailwind = 'block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-100 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-100 md:dark:hover:bg-transparent';
+  const munuTailwind = 'block py-2 px-3 text-gray-100 rounded hover:bg-cyan-100 md:hover:bg-transparent md:border-0 md:hover:bg-cyan-900 md:p-2 dark:text-gray-100 dark:hover:text-gray-100';
 
   const menus = [
     {
       menuName: 'Home',
-      menuStyle: munuTailwindHome,
+      menuStyle: munuTailwind,
       menuHref: '#'
     },
     {
@@ -33,7 +32,7 @@ const Menus: React.FC = () => {
   ]
 
   return(
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+      <ul className="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
         {menus.map((menu, index)=>(
           <li key={index}>
             <a href={menu.menuHref} className={menu.menuStyle}>{menu.menuName}</a>
