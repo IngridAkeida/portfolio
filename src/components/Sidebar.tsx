a781a71d1b860534f60e42a56ee76fb50625e037
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 09:01:36 +0100

    add - some elements inside sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,14 @@
 import { FaMedium } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import { SlSocialLinkedin } from "react-icons/sl";
+import { BiLogoGmail } from "react-icons/bi";
+import {IoLogoDribbble } from "react-icons/io";
+
 
 const Sidebar: React.FC = () => {
 
   const styleLi = "bg-cyan-950 p-2 sm:p-4 rounded-xl";
-  const styleA = "text-gray-300 hover:text-white mb-6";
-  const styleI = "fab fa-linkedin-in";
+  const styleA = "text-gray-300 hover:text-white mb-4 md:mb-6";
 
   const externalLinks = [
     { 
@@ -15,25 +17,36 @@ const Sidebar: React.FC = () => {
       icon: SlSocialLinkedin,
     }, 
     { 
+      nameLink: 'GitHub',
+      link: "https://github.com/",
+      icon: FiGithub,
+    }, 
+    { 
       nameLink: 'Medium',
       link: "https://medium.com/",
       icon: FaMedium,
     }, 
     { 
-      nameLink: 'GitHub',
-      link: "https://github.com/",
-      icon: FiGithub,
-    }, 
+      nameLink: 'Email',
+      link: "https://dribbble.com/IngriDiz",
+      icon: BiLogoGmail,
+    },
+    { 
+      nameLink: 'Drinbble',
+      link: "https://dribbble.com/IngriDiz",
+      icon: IoLogoDribbble,
+    },
   ] 
 
   return (
-    <div className="h-screen ml-4
-     sm:w-32 flex flex-col items-center justify-center fixed ">
-      <ul className=" flex flex-col gap-4">
+    <div className="md:h-screen ml-4 sm:w-32 flex md:flex-col items-center md:justify-center md:fixed">
+      <ul className=" flex md:flex-col  gap-2 md:gap-4">
         {externalLinks.map(( link, index)=>(
           <li key={index} className={styleLi}>
             <a href={link.link} target="_blank" rel="noopener noreferrer" className={styleA} aria-label={`Access my ${link.nameLink} using this link`}>
-              <span className={styleI}><link.icon size={28}/></span>
+              <span>
+                <link.icon size={18}/>
+              </span>
             </a>
           </li>
         ))}
