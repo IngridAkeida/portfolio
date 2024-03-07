dc38bc70662087016c55588b1e428dbf0bd2f281
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-07 09:00:39 +0100

    add - overview component

---
+++ src/components/Overview.tsx
@@ -0,0 +1,13 @@
+const Overview: React.FC = () => {
+  return (
+    <div className='flex flex-col gap-4 items-center mx-2'>
+      <div className='flex flex-col'>
+        <p className=''>
+        From the corporate world to the Tech realm, my journey began. Studying at Hyper Island, freelancing, and mastering React and TypeScript, I seek collaboration and adventure. With a love for cats and horror movies, I infuse my projects with playful surprises. This is the tale of my transformation and the endless possibilities of destiny⭐️!</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Overview;
