5cd4bc79c809b4aec1f28ecefb73275689db317a
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-08 17:34:32 +0100

    update commit

--- src/components/ui-components/Menus.tsx
+++ src/components/ui-components/Menus.tsx
@@ -1,6 +1,6 @@
 const Menus: React.FC = () => {
 
-  const munuTailwind = 'block py-2 px-3 text-gray-100 dark:hover:text-cyan-950 rounded hover:bg-cyan-100 md:hover:bg-transparent md:border-0 md:hover:bg-cyan-900 md:p-2 dark:text-gray-100 dark:hover:text-cyan-950';
+  const munuTailwind = 'block py-2 px-3 text-gray-100  md:border-0 md:p-2 dark:hover:text-cyan-950 hover:bg-cyan-800  dark:hover:bg-[#cededc] rounded-md';
 
   const menus = [
     {
@@ -30,7 +30,7 @@ const Menus: React.FC = () => {
   return(
       <ul className="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
         {menus.map((menu, index)=>(
-          <li key={index} className="hover:bg-cyan-800 md:hover:animate-wiggle animate-fill-forwards  dark:hover:bg-[#cededc] rounded-md">
+          <li key={index} className="md:hover:animate-wiggle animate-fill-forwards">
             <a href={menu.menuHref} className={menu.menuStyle} target={menu.menuRedirect}>{menu.menuName}</a>
           </li>
         ))}
