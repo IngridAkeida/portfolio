6b0685a405fce616d88fe5236a9c0e12c658066e
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-08 17:15:56 +0100

    Update Main and Overview components

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -30,11 +30,11 @@ const Main: React.FC = () => {
       <div className="col-span-5">
         <div
           id="page1"
-          className="container h-screen flex flex-col gap-4 justify-center items-center"
+          className="container h-screen flex flex-col gap-2 md:gap-4 justify-center items-center"
         >
           <Intro />
           <Overview />
-          <div className="absolute bottom-4">
+          <div className="absolute bottom-0">
             <button
               className="animate-bounce mt-4 bg-cyan-950 hover:bg-cyan-700 text-gray-100 py-2 px-4 rounded-md"
               onClick={scrollToProjects}
--- src/components/main/Overview.tsx
+++ src/components/main/Overview.tsx
@@ -1,7 +1,7 @@
 const Overview: React.FC = () => {
   return (
     <div className='flex flex-col gap-4 items-center mx-2 md:w-full xl:w-3/4'>
-      <div className='flex flex-col'>
+      <div className='flex flex-col -mb-8'>
         <p className=''>
         From the corporate world to the Tech realm, my journey began. Studying at Hyper Island, freelancing, and mastering React and TypeScript, I seek collaboration and adventure. With a love for cats and horror movies, I infuse my projects with playful surprises. This is the tale of my transformation and the endless possibilities of destiny⭐️!</p>
       </div>
