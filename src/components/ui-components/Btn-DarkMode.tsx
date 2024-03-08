38d101ecfb3282c404cda95e95cd304b8043e9a0
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-08 17:20:03 +0100

    Fix styling issues in Overview and Btn-DarkMode components

--- src/components/ui-components/Btn-DarkMode.tsx
+++ src/components/ui-components/Btn-DarkMode.tsx
@@ -11,7 +11,7 @@ const BtnDarkMode: React.FC<BtnDarkModeProps> = ({ toggleDarkMode, darkMode }) =
     <button 
       id="theme-toggle"
       type="button"
-      className="text-sm px-4 py-2 m-4 mr-6 rounded-md border-cyan-500 border-2
+      className="md:hover:animate-bounce text-sm px-4 py-2 m-4 mr-6 rounded-md border-cyan-500 md:border-2
       bg-cyan-950 text-blue-100 hover:bg-cyan-800 
       dark:bg-cyan-700 dark:text-yellow-500 dark:hover:bg-cyan-600
       focus:outline-none focus:ring-2 focus:ring-blue-400" 
