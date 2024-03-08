13ce3fe2bcff108ec10b1d01beaea00f07c835c0
IngridAkeida <ingridsanchesp@gmail.com>
2024-03-08 17:38:04 +0100

    Update Logo and Btn-DarkMode styles

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo: React.FC = () => {
   return (
-    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse hover:animate-shake">
+    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse hover:animate-shake hover:animate-infinite">
       <span className="flex text-cyan-900 bg-[#cededc] dark:text-gray-100 dark:bg-cyan-900 text-md font-semibold whitespace-nowrap p-2 rounded-md text-xl">
         {`Ingrid's Portfolio`}
       </span>
