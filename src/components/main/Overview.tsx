6035c636d15c73c0eb181a50ab421cae5211d519
IngridAkeida <ingridsanchesp@gmail.com>
2024-04-27 15:56:18 +0200

    update commit

--- src/components/main/Overview.tsx
+++ src/components/main/Overview.tsx
@@ -3,7 +3,7 @@ const Overview: React.FC = () => {
     <section className='flex flex-col gap-4 items-center mx-2 md:w-full xl:w-3/4'>
       <div className='flex flex-col mb-8'>
         <p>
-        Even though my background is in administration, I've found my passion as a Front-End Developer. I love working on visually appealing websites and meeting tight deadlines. I'm known for my attention to detail and organization skills. As a team player, I'm dedicated to making projects successful. With expertise in HTML, CSS, and JavaScript, I'm ready to create outstanding digital experiences.⭐️!</p>
+        Even though my background is in administration, I've found my passion as a Front-End Developer. I love working on visually appealing websites and meeting tight deadlines. I'm known for my attention to detail and organization skills. As a team player, I'm dedicated to making projects successful. With expertise in HTML, CSS, and JavaScript, I'm ready to create outstanding digital experiences⭐️.</p>
       </div>
 
     </section>
