5e29f6898d582567092aae590962e52d5b0e95a3
IngridAkeida <ingridsanchesp@gmail.com>
2024-05-13 17:54:18 +0200

    style - add flip card

--- src/components/main/CardProjects.tsx
+++ src/components/main/CardProjects.tsx
@@ -27,9 +27,9 @@ const CardProjects: React.FC <ProjectProps> = () => {
       repositorieProject:
         "https://github.com/IngridAkeida/react-hyperflix-movies-IngridFork",
       aboutProject:
-        "I dedicated myself to embracing new experiences, which led to an enhanced proficiency in ReactJS, utilizing it at advanced levels of complexity, and gaining practical experience with TailwindCSS. They also recognized the significance of utilizing APIs efficiently, understanding how to retrieve and integrate data from APIs into applications effectively.",
+        `I dedicated myself to mastering ReactJS and TailwindCSS, advancing to complex levels and efficiently utilizing APIs to integrate data into applications.`,
       buildProject: IoIosPeople,
-      technologies: ["ReactJs", "css"],
+      technologies: ['#reactJs', '#talwindcss', '#react-icons', '#api'],
     },
     {
       nameProject: "Pet Finder",
@@ -37,9 +37,9 @@ const CardProjects: React.FC <ProjectProps> = () => {
       siteProject: "https://petfinder-ingridsanches.netlify.app/",
       repositorieProject: "https://github.com/IngridAkeida/petfinder",
       aboutProject:
-        "Pet Finder is a web development passion project driven by a love for animals. The team focused on creating a platform that promotes pet adoption while prioritizing accessibility and SEO. They implemented features such as tags, attributes, and color contrast optimization to ensure easy navigation for all users. Additionally, SEO best practices were integrated throughout, including metadata optimization and structured data markup.",
+        `Pet Finder, a passion project, prioritizes accessibility and SEO, promoting pet adoption with features like tags, attributes, and SEO best practices.`,
       buildProject: IoIosPeople,
-      technologies: ["ReactJs", "css"],
+      technologies: ["#reactJs", "#css"],
     },
     {
       nameProject: "Age Calculator",
@@ -47,18 +47,18 @@ const CardProjects: React.FC <ProjectProps> = () => {
       siteProject: "https://agecalculator-2023.netlify.app/",
       repositorieProject: "https://github.com/IngridAkeida/age-calculator-app-main",
       aboutProject:
-        "Crafting an age calculator using JavaScript, CSS, and HTML was a journey of growth. I aimed for seamless validation and responsive design, ensuring an optimal user experience on any device. Through this project, I honed my skills and learned the importance of attention to detail and perseverance.",
+        `In this project I prioritized seamless validation and responsive design for optimal user experience. This project honed my skills and taught me the value of attention to detail and perseverance.`,
       buildProject: MdPerson2,
-      technologies: ["ReactJs", "css"],
+      technologies: ["#reactJs", "#css"],
     },
     {
       nameProject: "Calculator",
       imgProject: "/assets/projects/4.png",
       siteProject: "https://calcz.netlify.app/",
       repositorieProject: "https://github.com/IngridAkeida/calculadora",
-      aboutProject: `Building Calcz was a journey of discovery. Through JavaScript, HTML and Css, I learned to navigate challenges, embrace failures, and celebrate victories. Each line of code was a step forward, each bug a lesson learned. With Calcz, I ${`didn't`} just create a calculator—I uncovered my potential, honed my skills, and found joy in the process of creation.`,
+      aboutProject: `Building Calcz was a discovery journey in JavaScript, HTML, and CSS, navigating challenges, embracing failures, and celebrating victories. It uncovered my potential, honed skills, and brought joy in creation.`,
       buildProject: MdPerson2,
-      technologies: ["ReactJs", "css"],
+      technologies: ["#reactJs", "#css"],
     },
     {
       nameProject: 'Mentatt',
@@ -67,14 +67,14 @@ const CardProjects: React.FC <ProjectProps> = () => {
       repositorieProject:'https://github.com/IngridAkeida',
       aboutProject: `Mentatt expands to Eastern Africa with a user-centric webpage featuring services, success stories, and Imigongo art. Through interviews, prototypes, and linguistic inclusivity, we ensure an engaging and culturally rich experience for all users.`,
       buildProject: IoIosPeople,
-      technologies: [ 'ReactJs', 'talwindcss', 'nextjs', 'typescript', 'i18next', 'react-icons']
+      technologies: [ '#reactJs', '#talwindcss', '#nextjs', '#i18next', '#react-icons', '#next.js', '#storybook', '#jest.js', '#i18next', '#i18nexus', '#trello', '#figma']
     },
     // {
     //   nameProject: 'Find My GitHub',
     //   imgProject: '/assets/projects/5.png',
     //   siteProject: '',
     //   repositorieProject:'',
-    //   aboutProject:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut volutpat mauris. Nam facilisis ipsum eget urna congue, ac sodales nunc vulputate. Phasellus sit amet sapien at dui eleifend tincidunt. Nulla facilisi. Aenean auctor ipsum non risus vehicula, ac malesuada mi fermentum. Pellentesque euismod leo nec nulla bibendum, vel tristique felis facilisis. Suspendisse auctor enim vitae nunc posuere, vitae tempus mauris luctus.',
+    //   aboutProject:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     //   buildProject: MdPerson2,
     //   technologies: [ 'ReactJs', 'css']
     // }
@@ -87,42 +87,43 @@ const CardProjects: React.FC <ProjectProps> = () => {
           key={index}
           className="flex gap-4 bg-[#709994] border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 h-auto max-w-96 rounded-lg"
         >
-          <div className="relative">
-            <div className="rounded-t-lg overflow-hidden mt-4">
-              <Image
-                src={project.imgProject}
-                alt={`screenshot of the project ${project.nameProject}`}
-                width={250}
-                height={200}
-                objectFit="contain"
-                layout="responsive"
-              />
-            </div>
-            <div className="p-5">
-              <div>
-                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {project.nameProject}
-                </h2>
-                <project.buildProject />
+          <div className="group h-96 w-96 [perspective:1000px]">
+            <div className="relative text-center h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+              <div className="abosulte inset-0 text-white">
+                <h2 className="my-2 text-2xl font-bold tracking-tight">{project.nameProject}</h2>
+                <Image
+                  src={project.imgProject}
+                  alt={`screenshot of the project ${project.nameProject}`}
+                  width={250}
+                  height={200}
+                  objectFit="contain"
+                  layout="responsive"
+                  className="h-full w-full object-contain shadow-xl shadow-black/40"
+                />
+                <div className="p-2">{project.technologies.join(' ')}</div>
               </div>
-              <p className="mb-10 font-normal text-gray-900 dark:text-gray-400">
-                {project.aboutProject}
-              </p>
-              <div className="flex absolute bottom-4 gap-4">
-                <a
-                  href={project.siteProject}
-                  className={styleLink}
-                  target="_blank"
-                >
-                  Site
-                </a>
-                <a
-                  href={project.repositorieProject}
-                  className={styleLink}
-                  target="_blank"
-                >
-                  GitHub
-                </a>
+              <div className="absolute inset-0 h-full w-full rounded-xl bg-[#709994] dark:bg-gray-800 px-8 text-center text-slate-200 dark:text-slate-900 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                <div className="flex mt-2 flex-col items-center justify-center text-white">
+                  <h2 className="mb-2 text-2xl font-bold tracking-tight text-white">{project.nameProject}</h2>
+                  <project.buildProject />
+                  <p className="mb-10 font-normal">{project.aboutProject}</p>
+                  <div className="flex absolute bottom-4 gap-4">
+                    <a
+                      href={project.siteProject}
+                      className={styleLink}
+                      target="_blank"
+                    >
+                      Site
+                    </a>
+                    <a
+                      href={project.repositorieProject}
+                      className={styleLink}
+                      target="_blank"
+                    >
+                      GitHub
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
