92893ca5bd27808bf88283b866955a126e105055
IngridAkeida <ingridsanchesp@gmail.com>
2024-05-14 16:56:05 +0200

    update commit

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -2,7 +2,7 @@ const Logo: React.FC = () => {
   return (
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse sm:hover:animate-shake hover:animate-infinite">
       <span className="flex text-cyan-900 bg-[#cededc] dark:text-gray-100 dark:bg-cyan-900 text-md font-semibold whitespace-nowrap p-2 rounded-md text-xl">
-        {`Ingrid's Portfolio`}
+        {`iS`}
       </span>
     </a>
   );
