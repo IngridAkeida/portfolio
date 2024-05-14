b9180941b8f689ead5ff46992e73932eb3d7e483
IngridAkeida <ingridsanchesp@gmail.com>
2024-05-14 15:45:15 +0200

    delete - animation for mobile version

--- src/components/ui-components/Btn-ContratMe.tsx
+++ src/components/ui-components/Btn-ContratMe.tsx
@@ -1,6 +1,6 @@
 const BtnContratMe: React.FC = () => {
   return (
-    <button className="animate-wiggle animate-fill-forwards animate-infinite bg-cyan-950 hover:bg-cyan-700 text-gray-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+    <button className="md:animate-wiggle animate-fill-forwards animate-infinite bg-cyan-950 hover:bg-cyan-700 text-gray-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
       onClick={() => window.open("mailto:ingridsanchesp@gmail.com", "_blank")}>
       Email Me 📧
     </button>
